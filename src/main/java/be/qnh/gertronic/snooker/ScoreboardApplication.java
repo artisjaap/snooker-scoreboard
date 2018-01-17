a37af545ef086636260995dbccbe4e5234c6da2a
stijn <stijn.coene@qnh.be>
2018-01-17 18:49:21 +0100

    start lesson 1

---
+++ src/main/java/be/qnh/gertronic/snooker/ScoreboardApplication.java
@@ -0,0 +1,12 @@
+package be.qnh.gertronic.snooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreboardApplication.class, args);
+	}
+}
