9c56e93d9e555786602ef179040752ad6e83834e
Stijn Coene <coene.stijn@gmail.com>
2018-01-24 16:55:34 +0100

    test implementation of websockets

---
+++ src/main/java/be/qnh/gertronic/snooker/action/to/PointsWsRequest.java
@@ -0,0 +1,14 @@
+package be.qnh.gertronic.snooker.action.to;
+
+public class PointsWsRequest {
+
+    private Integer points;
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,51 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/matchsocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/currentframe/' + $("#matchId").val(), function (response) {
+            showGreeting(response);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/match/" + $("#matchId").val(), {}, JSON.stringify({'points': $("#points").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
+
